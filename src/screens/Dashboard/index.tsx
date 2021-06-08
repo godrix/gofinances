c89c8fc38974acb09a04502118f9393ac2598439
Carlos Gabriel Godri <08508575971@itajai.local>
2021-06-08 09:58:50 -0300

    [feature] component user

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import { View } from 'react-native';
 
-import { Container } from './styles';
+import { Container, Header, User, UserInfo, UserGreeting, UserImage, UserName, UserWrapper } from './styles';
 
 
 export function Dashboard() {
   return (
     <Container>
-      
+      <Header>
+        <UserWrapper>
+          
+        <UserInfo>
+          <UserImage source={{uri:'https://github.com/godrix.png'}}/>
+          <User>
+            <UserGreeting>
+              Ola,
+            </UserGreeting>
+            <UserName>
+              Fulano
+            </UserName>
+          </User>
+        </UserInfo>
+
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
