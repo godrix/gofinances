d8cfa74d9e9dc79230ddfb16b7bb7d4e8be16dda
Carlos Godri <cggodri@gmail.com>
2021-06-17 21:29:35 -0300

    customizado tabnavigation

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { View } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Dashboard } from '../screens/Dashboard';
 import { Register } from '../screens/Register';
+import {useTheme} from 'styled-components';
+import { Platform } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 const {Navigator, Screen} = createBottomTabNavigator();
 
@@ -10,15 +13,57 @@ const {Navigator, Screen} = createBottomTabNavigator();
 
 
 export function AppRoutes() {
+  const theme = useTheme();
   return (
-    <Navigator>
+    <Navigator
+    tabBarOptions={{
+      activeTintColor:theme.colors.secundary,
+      inactiveTintColor:theme.colors.text,
+      labelPosition:'beside-icon',
+      style:{
+        paddingVertical: Platform.OS === 'ios'?20:0,
+        height: RFValue(60)
+      }
+    }}
+    >
       <Screen
       name="Listagem"
       component={Dashboard}
+      options={{
+        tabBarIcon:({size, color})=>(
+          <MaterialIcons 
+          name="format-list-bulleted"
+          size={size}
+          color={color}
+          />
+        )
+      }}
       />
       <Screen
       name="Cadastro"
       component={Register}
+      options={{
+        tabBarIcon:({size, color})=>(
+          <MaterialIcons 
+          name="attach-money"
+          size={size}
+          color={color}
+          />
+        )
+      }}
+      />
+       <Screen
+      name="Resumo"
+      component={Register}
+      options={{
+        tabBarIcon:({size, color})=>(
+          <MaterialIcons 
+          name="pie-chart"
+          size={size}
+          color={color}
+          />
+        )
+      }}
       />
     </Navigator>
   );
